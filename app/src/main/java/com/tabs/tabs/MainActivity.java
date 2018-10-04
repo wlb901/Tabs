e8f79b05102876b1ce165e7e15f910b45613711d
Will Ballard <wlb901@gmail.com>
2018-10-04 14:11:14 -0500

    added tabs

--- app/src/main/java/com/tabs/tabs/MainActivity.java
+++ app/src/main/java/com/tabs/tabs/MainActivity.java
@@ -3,11 +3,94 @@ package com.tabs.tabs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tabs.tabs.R;
+import com.tabs.tabs.fragments.EightFragment;
+import com.tabs.tabs.fragments.NineFragment;
+import com.tabs.tabs.fragments.OneFragment;
+import com.tabs.tabs.fragments.SevenFragment;
+import com.tabs.tabs.fragments.SixFragment;
+import com.tabs.tabs.fragments.TenFragment;
+import com.tabs.tabs.fragments.ThreeFragment;
+import com.tabs.tabs.fragments.TwoFragment;
+import com.tabs.tabs.fragments.FourFragment;
+import com.tabs.tabs.fragments.FiveFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "ONE");
+        adapter.addFragment(new TwoFragment(), "TWO");
+        adapter.addFragment(new ThreeFragment(), "THREE");
+        adapter.addFragment(new FourFragment(), "Four");
+        adapter.addFragment(new FiveFragment(), "Five");
+        adapter.addFragment(new SixFragment(), "SIX");
+        adapter.addFragment(new SevenFragment(), "SEVEN");
+        adapter.addFragment(new EightFragment(), "EIGHT");
+        adapter.addFragment(new NineFragment(), "NINE");
+        adapter.addFragment(new TenFragment(), "TEN");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
